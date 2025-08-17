3432c1e586018bc2ab7aff14a516a0c786abeec6
salah <salahalhimyari2023@gmail.com>
2025-08-17 20:52:29 +0300

    feat(book): implement create book functionality

--- models/Book.php
+++ models/Book.php
@@ -17,6 +17,25 @@ class Book {
         $stmt = $this->db->query("SELECT * FROM books");
         return $stmt->fetchAll();
     }
+
+    //Add new book
+    public function create($data) {
+        $query = "INSERT INTO books (title, author, isbn, available_copies, total_copies) 
+                VALUES (:title, :author, :isbn, :available_copies, :total_copies)";
+        
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([
+            ':title' => $data['title'],
+            ':author' => $data['author'],
+            ':isbn' => $data['isbn'],
+            ':available_copies' => $data['total_copies'],
+            ':total_copies' => $data['total_copies']
+        ]);
+        
+        $this->logAction('Book created', "Title: {$data['title']}");
+        
+        return $this->db->lastInsertId();
+    }
     
     
 }
