ff2dbca8a48ed5afd43852e7af12edcc8b2ac8cc
salah <salahalhimyari2023@gmail.com>
2025-08-17 21:09:51 +0300

    feat(book): implement update book functionality

--- models/Book.php
+++ models/Book.php
@@ -36,6 +36,32 @@ class Book {
         
         return $this->db->lastInsertId();
     }
+
+    //Update book details
+    public function update($id, $data) {
+        $query = "UPDATE books SET  title = :title, 
+                                    author = :author, 
+                                    isbn = :isbn, 
+                                    available_copies = :available_copies,
+                                    total_copies = :total_copies
+                                    WHERE id = :id";
+        
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([
+            ':id' => $id,
+            ':title' => $data['title'],
+            ':author' => $data['author'],
+            ':isbn' => $data['isbn'],
+            ':available_copies' => $data['available_copies'],
+            ':total_copies' => $data['total_copies']
+        ]);
+        
+        $this->logAction('Book updated', "ID: $id");
+        
+        return $stmt->rowCount();
+    }
+
+    
     
     
 }
